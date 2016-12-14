dbd2f2eb3b9801a6992788b830ec2396d6d12cb6
fdj <janviernguyen@gmail.com>
2016-12-14 08:57:20 +0700

    start working

---
+++ Assets/Editor/Windows/TreeEditorWindow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TreeEditorWindow : EditorWindow {
+	public static TreeEditorWindow currentWindow;
+
+	// TODO: data struct with scriptableobject
+	[MenuItem ("Window/TreeEditor")]
+	public static void ShowTreeEditorWindow () {
+		currentWindow = (TreeEditorWindow)EditorWindow.GetWindow <TreeEditorWindow> ();
+
+	}
+	void OnEnable () {
+
+	}
+	void OnFocus () {
+
+	}
+
+	void OnGUI() {
+
+	}
+}
